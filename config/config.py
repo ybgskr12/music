c405eea0b3440ac35548e576e823718116280256
ʙᴧɢᴧsҡᴧʀᴧ-ᴇx <93908715+ybgskr12@users.noreply.github.com>
2023-08-22 18:46:20 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -124,11 +124,11 @@ CLEANMODE_DELETE_MINS = int(
 # Telegram audio  and video file size limit
 
 TG_AUDIO_FILESIZE_LIMIT = int(
-    getenv("TG_AUDIO_FILESIZE_LIMIT", "10485760000")
+    getenv("TG_AUDIO_FILESIZE_LIMIT", "5510485760000")
 )  # Remember to give value in bytes
 
 TG_VIDEO_FILESIZE_LIMIT = int(
-    getenv("TG_VIDEO_FILESIZE_LIMIT", "10737410824")
+    getenv("TG_VIDEO_FILESIZE_LIMIT", "5550737410824")
 )  # Remember to give value in bytes
 
 # Chceckout https://www.gbmb.org/mb-to-bytes  for converting mb to bytes
